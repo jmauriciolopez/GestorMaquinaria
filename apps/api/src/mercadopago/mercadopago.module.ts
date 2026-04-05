f15396509b17c9002dc6cce5a76a49fb459812aa
Mauricio Lopez <jmauricio_lopez@hotmail.com>
2026-04-05 01:51:35 -0300

    feat(mercadopago): importar NotificacionesModule

--- apps/api/src/mercadopago/mercadopago.module.ts
+++ apps/api/src/mercadopago/mercadopago.module.ts
@@ -5,9 +5,13 @@ import { MercadoPagoController } from './mercadopago.controller';
 import { Pago } from '../pagos/pago.entity';
 import { Alquiler } from '../alquileres/alquiler.entity';
 import { AlquilerItem } from '../alquileres/alquiler.entity';
+import { NotificacionesModule } from '../notificaciones/notificaciones.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pago, Alquiler, AlquilerItem])],
+  imports: [
+    TypeOrmModule.forFeature([Pago, Alquiler, AlquilerItem]),
+    NotificacionesModule,
+  ],
   controllers: [MercadoPagoController],
   providers: [MercadoPagoService],
   exports: [MercadoPagoService],
